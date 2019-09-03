385523df1c043b6cd079716cb71491e9e638d7fe
Maria E <mariaeugeniamaciel@gmail.com>
2019-09-02 21:25:58 -0300

    ajax_xml

--- maeh/cod.js
+++ maeh/cod.js
@@ -1,17 +1,36 @@
 
-function ajax() {
+function ajax(recurso,funcao) {
     var ajax = new XMLHttpRequest();
-    ajax.onreadystatechange = mostrar;
-    ajax.open("GET", "ajax_info.txt", true);
+    ajax.onreadystatechange = funcao
+    ajax.open("GET",recurso, true);
     ajax.send();
+    //POST ajax.send(dados)
+    //var parser= new DOMParser();
   }
 
-  function mostrar(){
+
+  function mostrar()
+  {if(this.readyState==4 && this.status==200){
+      var parser = new DOMParser();
+      var xml = parser.parseFromString(this.responseText,"text/xml");
+      var raiz=xml.documentElement;
+      var filhos=raiz.childNodes;
+      
+       
+
     
-    if(this.readyState==4 && this.status==200){
-    document.getElementById('resp').innerHTML=this.responseText
-}
+      for(let filho of filhos)
+      { document.getElementById('resp').innerHTML+=filho.nodeName
+        if(filho.nodeType==1){
+          document.getElementById('resp').innerHTML+=":"+filho.textContent
+        }
+        document.getElementById('resp').innerHTML+='<br>'
+      }
+  }
   }
 
-  //cors recursos de outro dominio
-  document.getElementById('r').onclick=ajax
+onload=function(){
+    ajax("nota.xml",mostrar)
+  }
+
+  
